fd7412fb57d816570c6fd5846e5c4498764756c6
icrs1990 <isabel.ramirez.sierra@gmail.com>
2025-02-08 17:45:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-document.querySelectorAll(".plan button").forEach(button => {
-    button.addEventListener("click", () => {
-        alert("Gracias por tu interés. Te contactaremos pronto.");
-    });
-});
-
 document.querySelector(".boton-general").addEventListener("click", () => {
     alert("Déjanos tu contacto y te daremos más información.");
 });
+
