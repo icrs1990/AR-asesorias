719137a03c1786da80017bac325c7fc669bc80ed
icrs1990 <isabel.ramirez.sierra@gmail.com>
2025-02-08 17:06:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".plan button").forEach(button => {
+    button.addEventListener("click", () => {
+        alert("Gracias por tu interés. Te contactaremos pronto.");
+    });
+});
